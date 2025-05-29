39d0ffc404547739d22699ab99db62729c18f283
itc-n25016 <n25016@std.it-college.ac.jp>
2025-05-29 02:16:03 +0000

    P88-P101 第6章演習 終了

---
+++ Chapter06/ex6-09.py
@@ -0,0 +1,6 @@
+print("こんにちは、{}".format("ウィリアム・フォーグナー"))
+name = "ウィリアム・フォーグナー"
+print("こんにちは、{}".format(name))
+author = "ウィリアム・フォーグナー"
+year_born = "1897"
+print("{}は{}年に生まれました。".format(author,year_born))
